027b24afa60b4494ed1b7f4e29e445988c20e57b
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-06-18 20:58:11 -0500

    initial commit

---
+++ functions/users.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+// Array of users
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Bob Johnson" },
+];
+
+// Get all users
+app.get("/", (req, res) => {
+  res.json(users);
+});
+
+// Get a specific user by ID
+app.get("/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((user) => user.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+// Array of users
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Bob Johnson" },
+];
+
+// Get all users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Get a specific user by ID
+app.get("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((user) => user.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
